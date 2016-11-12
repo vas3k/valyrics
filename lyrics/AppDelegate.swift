606381e9475ec6bb1a266eebb491138745053e21
vas3k <me@vas3k.ru>
2016-11-12 19:32:35 +0200

    Initial Commit

---
+++ lyrics/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  lyrics
+//
+//  Created by vas3k on 12/11/2016.
+//  Copyright © 2016 vas3k. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ lyrics/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lyrics
+//
+//  Created by vas3k on 12/11/2016.
+//  Copyright © 2016 vas3k. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
